60d48b69eced5ea52c5e8c168c18b60021ccbe5c
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-09-19 09:14:21 +0530

    Added: Employee is Present/Absent

--- src/com/employeewage/EmpWage.java
+++ src/com/employeewage/EmpWage.java
@@ -2,9 +2,20 @@ package com.employeewage;
 
 
 public class EmpWage {
+	public final int is_presentemployee=1;
+	public int attendemployee=(int) (Math.floor(Math.random()*10)%2);
 
+	public void EmpWageAttendance()
+	{	
+		if (attendemployee==is_presentemployee) {
+			System.out.println("Employee is Present");}
+		else {
+			System.out.println("Employee is Absent");
+		}
+	}
 	public static void main(String[] args) {
-		System.out.println("Welcome To The Employee Wage");
+		EmpWage Attend=new EmpWage();
+		Attend.EmpWageAttendance();
 
 	}
 }
