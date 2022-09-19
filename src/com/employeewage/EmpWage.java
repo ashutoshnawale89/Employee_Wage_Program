50216694ec96e89b243e8a109a8422f05038bfb2
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-09-19 09:48:14 +0530

    Added: Wage of Month

--- src/com/employeewage/EmpWage.java
+++ src/com/employeewage/EmpWage.java
@@ -6,29 +6,37 @@ public class EmpWage {
 	public int emphrs;
 	public int employeewage;
 	public final int is_presentemployee=1;
+	public final int workingdays=20;
+	int n=1;
+	int totalempwage=0;
 	
 	public int attendemployee=(int) (Math.floor(Math.random()*10)%3);
 
 	public void EmpWageAttendance()
 	{	
+		while (n < workingdays)
+		{ n++;
 		int attendemployee=(int) (Math.floor(Math.random()*10)%3);
 
 		switch (attendemployee) {
 		case  1 :
 			emphrs = 8;
-			System.out.println("Employee is Present Full Time");
+			System.out.print("Employee is Present Full Time");
 			break;
 		case 2 :
 			emphrs = 4;
-			System.out.println("Employee is Present Part Time");
+			System.out.print("Employee is Present Part Time");
 			break;
 		default:
 			emphrs = 0;
-			System.out.println("The Employee is Absent");
+			System.out.print("The Employee is Absent");
 		}
 		employeewage=emphrs*wage_per_hr;
-
-		System.out.println("The Salary of Employee is "+employeewage+" ");
+		System.out.println(employeewage);
+		totalempwage=totalempwage+employeewage;
+		}
+		System.out.println("***********************************************************");
+		System.out.println("The Monthe Salary of Employee is "+totalempwage+" ");
 	}
 	public static void main(String[] args) {
 		EmpWage Attend=new EmpWage();
