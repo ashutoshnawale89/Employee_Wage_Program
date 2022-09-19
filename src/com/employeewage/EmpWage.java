727d05c7b288db50d5cc9d1d47c164efacf901af
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-09-19 09:39:36 +0530

    Added: Employee Wage is Slove Switch Case

--- src/com/employeewage/EmpWage.java
+++ src/com/employeewage/EmpWage.java
@@ -11,18 +11,21 @@ public class EmpWage {
 
 	public void EmpWageAttendance()
 	{	
-		if (attendemployee==is_presentemployee) {
-			emphrs=8;
-			System.out.println("Employee is Present Full Time");}
-		else if (attendemployee==2) {
+		int attendemployee=(int) (Math.floor(Math.random()*10)%3);
+
+		switch (attendemployee) {
+		case  1 :
+			emphrs = 8;
+			System.out.println("Employee is Present Full Time");
+			break;
+		case 2 :
+			emphrs = 4;
 			System.out.println("Employee is Present Part Time");
-			emphrs=4;
-		}
-		else {
-			emphrs=0;
+			break;
+		default:
+			emphrs = 0;
 			System.out.println("The Employee is Absent");
 		}
-			
 		employeewage=emphrs*wage_per_hr;
 
 		System.out.println("The Salary of Employee is "+employeewage+" ");
